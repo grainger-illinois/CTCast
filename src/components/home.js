8586bb081dce6ceb9872b51265aea74dcce0948b
taeingu2 <taeingu2@illinois.edu>
2022-04-29 13:57:43 -0400

    added cards in home page for instructions. Needs to add instruction
    Signed-off-by: taeingu2 <taeingu2@illinois.edu>

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,72 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+const zoom_card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        Zoom
+      </Typography>
+      
+      <Typography variant="body2">
+        Instructions
+        
+      </Typography>
+    </CardContent>
+  </React.Fragment>
+);
+
+const linkencoder_card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        Link Encoder
+      </Typography>
+      
+      <Typography variant="body2">
+        Instructions
+        
+      </Typography>
+    </CardContent>
+  </React.Fragment>
+);
+
+const upload_card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        File Upload
+      </Typography>
+      
+      <Typography variant="body2">
+        Instructions
+        
+      </Typography>
+    </CardContent>
+  </React.Fragment>
+);
 
 function home() {
   return (
-    <div><h2>Home</h2></div>
+    <div>
+      <h2>Welcome to ClassTranscribe!</h2>
+      <Box sx={{ minWidth: 275 }} style={{marginBottom: '1rem'}}>
+        <Card variant="outlined">{zoom_card}</Card>
+      </Box>
+
+      <Box sx={{ minWidth: 275 }} style={{marginBottom: '1rem'}}>
+        <Card variant="outlined">{linkencoder_card}</Card>
+      </Box>
+
+      <Box sx={{ minWidth: 275 }} style={{marginBottom: '1rem'}}>
+        <Card variant="outlined">{upload_card}</Card>
+      </Box>
+      
+
+    </div>
   )
 }
 
