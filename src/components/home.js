3e84ff79f0e9616b2ab3f7e1774cb4847dd9797e
Shubham Kumar <shubhamk008@yahoo.in>
2022-10-05 15:11:51 -0500

    Fix title

--- src/components/home.js
+++ src/components/home.js
@@ -91,7 +91,7 @@ const upload_card = (
 function home() {
   return (
     <div style={{ margin: "20px", marginTop: "30px" }}>
-      <h1 style={{ textAlign: "left" }}>Welcome to ClassTranscribe!</h1>
+      <h1 style={{ textAlign: "left" }}>Welcome to CTCast</h1>
       <Box sx={{ minWidth: 275 }} style={{ marginBottom: '1rem' }}>
         <Card variant="outlined">{zoom_card}</Card>
       </Box>
