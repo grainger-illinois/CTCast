051e4e36fe74c42a233558ba32ce6f2ccf17670d
RomeroRIsa <rrisa2@illinois.edu>
2022-10-11 17:49:43 -0500

    Changed test file

---
+++ src/test/spec.mjs
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+import { _electron } from 'playwright';
+
+test('App launches and quits', async () => {
+  test.setTimeout(0);
+  const app = await _electron.launch({args: ['.']});
+  const window = await app.firstWindow();
+  await expect(await window.title()).toContain('');
+  await app.close();
+});
