39ea736a526b34b48560b95497161b83329b8a3b
Timmy Sullivan <tps7@illinois.edu>
2022-10-21 20:32:43 -0500

    added basic tests. Can add more in the future

---
+++ testing/basic_tests_spec.ts
@@ -0,0 +1,164 @@
+import { expect, test, request, chromium } from '@playwright/test'
+import { 
+  clickMenuItemById,
+  findLatestBuild, 
+  ipcMainCallFirstListener, 
+  ipcRendererCallFirstListener, 
+  parseElectronApp,
+  ipcMainInvokeHandler,
+  ipcRendererInvoke
+  
+} from 'electron-playwright-helpers'
+
+import React from 'react'
+import { EventEmitter } from 'node:events'
+import { BrowserContext, ElectronApplication, Page, _electron as electron } from 'playwright'
+import { BrowserNotSupported, MessageSharp } from '@mui/icons-material'
+import LinkEncoder from '../src/components/LinkEncoder'
+
+let electronApp: ElectronApplication
+
+test.beforeAll(async () => {
+  // find the latest build in the out directory
+  const latestBuild = findLatestBuild()
+  // parse the directory and find paths and other info
+  const appInfo = parseElectronApp(latestBuild)
+  // set the CI environment variable to true
+  //process.env.CI = 'e2e'
+  electronApp = await electron.launch({
+    args: [appInfo.main],
+    executablePath: appInfo.executable
+  })
+
+  electronApp.on('window', async (page) => {
+    const filename = page.url()?.split('/').pop()
+    console.log(`Window opened: ${filename}`)
+
+    // capture errors
+    page.on('pageerror', (error) => {
+      console.error(error)
+    })
+    // capture console messages
+    page.on('console', (msg) => {
+      console.log(msg.text())
+    })
+  })
+
+})
+
+test.afterAll(async () => {
+  await electronApp.close()
+})
+
+let page: Page
+
+
+
+test('Sends Text Correctly', async () => {
+  page = await electronApp.firstWindow()
+  const [window] = await Promise.all([
+    page.waitForEvent('framenavigated'),
+    page.locator('a:has-text("Link Encoder")').click()
+  ]);
+
+  const ipAd = window.locator('label:has-text("IP Address")')
+
+  const port = window.locator('label:has-text("Port")')
+
+  const messageBox = window.locator('label:has-text("Message")')
+
+
+  await ipAd.type('127.0.0.1')
+  await port.type('10002')
+  console.log(await messageBox.inputValue())
+  await messageBox.type("hello")
+  const s2 = await messageBox.inputValue()
+  expect(s2).toBe("hello")
+  const r = window.locator('button:has-text("Submit")')
+  await r.click()
+  //await (window.waitForTimeout(2000))
+  //console.log('done')
+  
+
+ 
+})
+
+
+test('renders the home page', async () => {
+  page = await electronApp.firstWindow()
+  //await page.waitForNavigation()
+  await page.waitForSelector('h1')
+  //await page.waitForSelector('h1')
+  const text = await page.$eval('h1', (el) => el.textContent)
+  expect(text).toBe('Welcome to CTCast')
+  const title = await page.title()
+  expect(title).toBe('CTCast')
+})
+
+
+test('renders the zoom page', async () => {
+  page = await electronApp.firstWindow()
+  console.log(page.title())
+  const [window] = await Promise.all([
+    page.waitForEvent('framenavigated'),
+    page.locator('a:has-text("Zoom")').click()
+  ]);
+  await window.waitForSelector('h1')
+  const text = await page.$eval('h1', (el) => el.textContent)
+  expect(text).toBe('Zoom')
+  page = window.page()
+})
+
+
+test('renders the link encoder page', async () => {
+  page = await electronApp.firstWindow()
+  const [window] = await Promise.all([
+    page.waitForEvent('framenavigated'),
+    page.locator('a:has-text("Link Encoder")').click()
+  ]);
+  await window.waitForSelector('h1')
+  const text = await page.$eval('h1', (el) => el.textContent)
+  expect(text).toBe('Link Encoder')
+  page = window.page()
+})
+
+
+test('renders the file upload page', async () => {
+  page = await electronApp.firstWindow()
+  const [window] = await Promise.all([
+    page.waitForEvent('framenavigated'),
+    page.locator('a:has-text("File Upload")').click()
+  ]);
+  await window.waitForSelector('h1')
+  const text = await page.$eval('h1', (el) => el.textContent)
+  expect(text).toBe('File Upload')
+  page = window.page()
+})
+
+
+test('renders the help page', async () => {
+  page = await electronApp.firstWindow()
+  const [window] = await Promise.all([
+    page.waitForEvent('framenavigated'),
+    //page.locator('a:has-text("help")').click()
+    page.locator('id=help').click()
+  ]);
+  await window.waitForSelector('h1')
+  const text = await page.$eval('h1', (el) => el.textContent)
+  expect(text).toBe('Captioned Enhanced Audio Descriptions Best Practices')
+  page = window.page()
+})
+
+test('Correctly returns to home page', async () => {
+  page = await electronApp.firstWindow()
+  const [window] = await Promise.all([
+    page.waitForEvent('framenavigated'),
+    page.locator('a:has-text("Home")').click()
+  ]);
+  await window.waitForSelector('h1')
+  const text = await page.$eval('h1', (el) => el.textContent)
+  expect(text).toBe('Welcome to CTCast')
+  page = window.page()
+})
+
+
