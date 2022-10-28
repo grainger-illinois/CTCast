ff2cfd97219ea027274bcd1cc2b8c437a862d2ce
Timmy Sullivan <tps7@illinois.edu>
2022-10-28 16:47:16 -0500

    Fixed connection error with send text test case and updated README

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,14 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './testing',
   maxFailures: 2,
+
+
+  webServer: {
+    command: 'node testing_linkencoder_scripts/fake-link.js',
+    port: 10002
+    //url: 'http://localhost:10002',
+    //reuseExistingServer: false
+  },
 }
 
 export default config
