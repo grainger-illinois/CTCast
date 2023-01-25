9b6edb800b0c06f1fe2b4a035d43d77ff32470e0
RomeroRIsa <rrisa2@illinois.edu>
2023-01-25 14:51:19 -0600

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -183,6 +183,8 @@ export class LinkEncoderAPI {
         }
         else {
             this.socket.destroy();
+            this.newswire = null;
+            this.fieldinsertmode = null;
             console.log('Disconnected from ' + host + ':' + port);
         }
 
