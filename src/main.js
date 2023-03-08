dd9fddf7fd24681f77a58f3ea2034803883f008b
RomeroRIsa <rrisa2@illinois.edu>
2023-03-08 14:30:35 -0600

    Update window resolution

--- src/main.js
+++ src/main.js
@@ -79,13 +79,11 @@ async function fileProcessHandler(ext, arrayBuffer) {
   
 }
 
-
-
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 960,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       enableRemoteModule: true
